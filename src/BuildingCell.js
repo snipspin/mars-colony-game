9d9afb4a100884fb9eea0f6a07f874bcc82b6102
chris-jburke <cjburke04@gmail.com>
2020-04-05 16:21:56 -0700

    added some additional UI and styling for the buildings

--- src/BuildingCell.js
+++ src/BuildingCell.js
@@ -1,8 +1,25 @@
 import React, {useState, useEffect} from 'react'
-
+import {Box, Button} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
+import {green, blue, red} from '@material-ui/core/colors'
 const BuildingCell = (props) => {
 	const [localResource, setLocalResource] = useState(0)
 	const [increment, setIncrement] = useState(1)
+	const [resourceType, setResourceType] = useState('')
+	const [resourceTypeLevel, setResourceTypeLevel] = useState('')
+	const GreenButton = withStyles(theme => ({
+		root: {
+    		color: theme.palette.getContrastText(green[500]),
+    		borderColor: green[500],
+  		},
+	}))(Button);
+	const BlueButton = withStyles(theme => ({
+		root: {
+    		color: "black",
+    		borderColor: blue[500],
+  		},
+	}))(Button);
+
 //https://stackoverflow.com/questions/53395147/use-react-hook-to-implement-a-self-increment-counter
 	useEffect(() => {
 		const timeOut = setTimeout(() => {
@@ -16,6 +33,10 @@ const BuildingCell = (props) => {
 
 	useEffect(() => {
 		setLocalResource(0)
+		const typeClass = "resourceNumber" + props.type
+		const typeLevelClass = "resourceLevel" + props.type
+		setResourceType(typeClass)
+		setResourceTypeLevel(typeLevelClass)
 	},[])
 
 	const clickHandler = (e) => {
@@ -30,13 +51,42 @@ const BuildingCell = (props) => {
 		let amount = increment + 1
 		setIncrement(amount)
 	}
+	const getBuilding = () => {
+		if(props.type == "Food") {
+			return (
+				<Box style={{"display": "flex", "flexDirection": "column", "alignItems":"center", "width": "300px"}}>
+					<GreenButton variant="outlined" onClick={(e) => clickHandler(e)}>Harvest</GreenButton>
+					<span className={resourceType}>{localResource}</span>
+					<GreenButton variant="outlined" onClick={(e) => clickUpgradeHandler(e)}>Upgrade</GreenButton>
+					<span className={resourceTypeLevel}>Level: {increment}</span>
+				</Box>
+			)
+		} else if(props.type == "Water") {
+			return (
+				<Box style={{"display": "flex", "flexDirection": "column", "alignItems":"center", "width": "300px"}}>
+					<BlueButton variant="outlined" onClick={(e) => clickHandler(e)}>Harvest</BlueButton>
+					<span className={resourceType}>{localResource}</span>
+					<BlueButton variant="outlined" onClick={(e) => clickUpgradeHandler(e)}>Upgrade</BlueButton>
+					<span className={resourceTypeLevel}>Level: {increment}</span>
+				</Box>
+			)
+		}
+	}
+	const getUpgradeButton = () => {
+		if(props.type == "Food") {
+			return (
+				<GreenButton variant="outlined" onClick={(e) => clickUpgradeHandler(e)}>Upgrade</GreenButton>
+			)
+		} else{
+			return( 
+				<Button variant="outlined" onClick={(e) => clickUpgradeHandler(e)}>Upgrade</Button>
+			)
+		}
+	}
 	return (
-		<div>
-			<button onClick={(e) => clickHandler(e)}>Harvest</button>
-			<span>{localResource}</span>
-			<button onClick={(e) => clickUpgradeHandler(e)}>Upgrade</button>
-			<span>Level: {increment}</span>
-		</div>
+		<Box style={{"width":"300px"}}>
+			{getBuilding}
+		</Box>
 	)
 }
 export default BuildingCell
--- src/ResourceBar.js
+++ src/ResourceBar.js
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import {Grid, Box} from '@material-ui/core'
+import './App.css'
 const ResourceBar = (props) => {
 	return (
-		<div>
-			<ul>
-				<li>{props.water}</li>
-				<li>{props.food}</li>
-				<li>{props.people}</li>
-			</ul>
-		</div>
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems = "center"
+		>
+			<Grid item xs={12}>
+				<Box>
+					<span className="resourceNumberWater">{props.water}</span>
+					<span className="resourceNumberFood">{props.food}</span>
+					<span className="resourceNumberPeople">{props.people}</span>
+				</Box>
+			</Grid>
+		</Grid>
 	)
 }
 export default ResourceBar
