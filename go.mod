74bd3d800219053c8774ed903da039d8f53e3be4
chris-jburke <cjburke04@gmail.com>
2020-04-06 15:11:58 -0700

    small test that was reverted

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/snipspin/mars-colony-game
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.2 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
+)
--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"net/http"
 	"os"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	r.GET("/", func(c ))
 	fs := http.FileServer(http.Dir("build"))
 	http.Handle("/", fs)
 	http.ListenAndServe(":"+port, nil)
